d62c2dc4fc6cd69bdadd8522150b4f5d41d8ea9f
Kenyi Rodriguez <kenyi.rodriguez@globant.com>
2022-04-30 09:48:11 -0500

    Sesion 04

---
+++ Sesion04/Constraints1/Constraints1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Constraints1
+//
+//  Created by Kenyi Rodriguez on 30/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
